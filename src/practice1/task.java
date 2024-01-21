5646a0a88f4d76eda5cfc760ab4c1ab7e1112f43
takaya <145169232+takayaadachi@users.noreply.github.com>
2024-01-21 09:41:53 +0900

    課題提出

---
+++ src/practice1/task.java
@@ -0,0 +1,21 @@
+package practice1;
+
+import java.time.LocalDate;
+
+class Task {
+    private LocalDate date;
+    private String content;
+
+    public Task(LocalDate date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getTaskNaiyo() {
+        return content;
+    }
+}
