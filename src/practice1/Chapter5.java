32bb95ca12cbf8dfdaf0b3df2c3aad3082c14319
takaya <145169232+takayaadachi@users.noreply.github.com>
2024-01-21 09:54:12 +0900

    課題提出

--- src/practice1/Chapter5.java
+++ src/practice1/Chapter5.java
@@ -6,22 +6,20 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+public class Chapter5 {
 
-public class Chapter5{
-	
-	  public static void main(String[] args) {
-	        List<Task> tasks = new ArrayList<>();
-	        tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
-	        tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談"));
-	        tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
-	        tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
-	        tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
+	public static void main(String[] args) {
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"));
+		tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談"));
+		tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う"));
+		tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く"));
+		tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く"));
 
-	        Collections.sort(tasks, Comparator.comparing(Task::getDate));
+		Collections.sort(tasks, Comparator.comparing(Task::getDate));
 
-	        for (Task task : tasks) {
-	            System.out.println(task.getDate() + ":" + task.getTaskNaiyo());
-	        }
-	    }
+		for (Task task : tasks) {
+			System.out.println(task.getDate() + ":" + task.getTaskNaiyo());
+		}
 	}
-
+}
--- src/practice1/task.java
+++ src/practice1/task.java
@@ -3,19 +3,19 @@ package practice1;
 import java.time.LocalDate;
 
 class Task {
-    private LocalDate date;
-    private String content;
+	private LocalDate date;
+	private String content;
 
-    public Task(LocalDate date, String content) {
-        this.date = date;
-        this.content = content;
-    }
+	public Task(LocalDate date, String content) {
+		this.date = date;
+		this.content = content;
+	}
 
-    public LocalDate getDate() {
-        return date;
-    }
+	public LocalDate getDate() {
+		return date;
+	}
 
-    public String getTaskNaiyo() {
-        return content;
-    }
+	public String getTaskNaiyo() {
+		return content;
+	}
 }
